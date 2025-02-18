8fc6487ce1eddbf250e545420cf3a91cf2c9866c
Joydeep Nath <joydeepnath279@gmail.com>
2025-02-18 15:36:59 +0530

    added DayTwo

---
+++ lexical Env/main.js
@@ -0,0 +1,26 @@
+// function main() {
+//     var a = 10;
+//     test2();
+//     function test2() {
+//         console.log(a);
+//     }
+// }
+
+function main() {
+  console.log(window);
+}
+
+let x = 100;
+main();
+console.log(x);
+var temp = 20;
+var temp = 24;
+
+let test1 = 2;
+{
+  let test1 = 3;
+  console.log(test1);
+}
+
+console.log(test1);
+
